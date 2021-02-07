13364a74cbb42dee30704fbff000c0769693f0ab
px3l <px3l@tuta.io>
2021-02-07 11:31:01 +0000

    initial commit - whole project restarted with modern react

--- README.md
+++ README.md
@@ -1,48 +1,70 @@
-# Skyrim Quests Checklist
+# Getting Started with Create React App
 
-This is a checklist for every quest on Elder Scrolls V: Skyrim for 100% completion. It is build in React and reduxq
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Development
+## Available Scripts
 
-To build the frontend (outputs to `frontend/dist`), whilst triggering rebuilds on save:
+In the project directory, you can run:
 
-```bash
-$ yarn run watch
-```
+### `yarn start`
 
-#### Serve
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-To serve the frontend you can use the npm module "ecstatic"
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
 
-```bash
-$ cd dist
-$ ecstatic
-```
+### `yarn test`
 
-#### Clean or Reinstall
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-To clean `/dist` for rebuild use
+### `yarn build`
 
-```bash
-$ yarn run clean
-```
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
 
-To completely reinstall (delete `/node_modules` and `/dist`)
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
 
-```bash
-$ yarn run reinstall
-```
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-## Basic Use
+### `yarn eject`
 
-#### Table
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
 
-The quests are in a material-ui table and there is a simple state object. The config state which defines the table properties, header, etc. The selected state which saves a boolean for each table row to display it as either checked or not checked.
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-#### Saga
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
 
-TO DO
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
-#### Tests
+## Learn More
 
-TO DO
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `yarn build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/TableData.js
+++ src/TableData.js
@@ -1,173 +1,243 @@
+/*
+
+Structure is:
+
+{
+  ID: 'ID',                   // In game ID
+  name: 'name',               // Title of quest
+  description: 'description', // Description
+  giver: 'giver',             // NPC that starts the quest
+  location: 'location',       // Location of start
+  level: 1,                   // Player level required to start
+  reward: 'reward',
+  type: 'type',
+  link: 'link'
+},
+{
+  ID: 'ID',
+  name: 'name',
+  description: 'description',
+  giver: 'giver',
+  location: 'location',
+  level: 1,
+  reward: 'reward',
+  type: 'type',
+  link: 'link'
+},
+
+*/
+
 const tableData = {
   mainQuest: [
     {
-      quest: 'Unbound',
+      ID: 'MQ101',
+      name: 'Unbound',
       description: 'Escape Imperial custody and a dragon raid.',
-      giver: '',
-      optional: false
+      giver: 'N/A',
+      location: 'Helgen, Helgen Keep',
+      level: 1,
+      reward: 'Various low level items',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Unbound'
     },
     {
-      quest: 'Before the Storm',
+      ID: 'MQ102',
+      name: 'Before the Storm',
       description: 'Inform the Jarl of Whiterun about the dragon attack on Helgen.',
       giver: 'Hadvar or Ralof',
-      optional: false
+      location: 'Riverwood, Whiterun',
+      level: 1,
+      reward: 'Leveled Armor',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Before_the_Storm'
     },
     {
-      quest: 'Bleak Falls Barrow',
+      ID: 'MQ103',
+      name: 'Bleak Falls Barrow',
       description: 'Retrieve the Dragonstone.',
-      giver: 'Jarl Balgruuf',
-      optional: false
+      giver: 'Jarl Balgruuf the Greater',
+      location: 'Riverwood, Bleak Falls Barrow, Dragonsreach',
+      level: 1,
+      reward: 'Enchanted Leveled Armor, Permission to buy Breezehome',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Bleak_Falls_Barrow_(quest)'
     },
     {
-      quest: 'Dragon Rising',
+      ID: 'MQ104',
+      name: 'Dragon Rising',
       description: 'Investigate a Dragon sighting near Whiterun.',
-      giver: 'Jarl Balgruuf',
-      optional: false
+      giver: 'Farengar Secret-Fire',
+      location: 'Dragonsreach, The Western Watchtower outside Whiterun',
+      level: 1,
+      reward: 'Ability to use Dragon Shouts, Axe of Whiterun, Thaneship of Whiterun, Lydia as a follower',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Dragon_Rising'
     },
     {
-      quest: 'The Way of the Voice',
+      ID: 'MQ105',
+      name: 'The Way of the Voice',
       description: 'Meet the Greybeards.',
-      giver: 'Jarl Balgruuf',
-      optional: false
+      giver: 'Jarl Balgruuf the Greater',
+      location: 'Ivarstead, High Hrothgar',
+      level: 1,
+      reward: 'Word for the Unrelenting Force shout, Word for the Whirlwind Sprint shout',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:The_Way_of_the_Voice'
     },
     {
-      quest: 'The Horn of Jurgen Windcaller',
+      ID: 'MQ105Ustengrav',
+      name: 'The Horn of Jurgen Windcaller',
       description: 'Retrieve a Greybeard artifact from Ustengrav.',
       giver: 'Arngeir',
-      optional: false
+      location: 'High Hrothgar, Ustengrav, Riverwood',
+      level: 1,
+      reward: 'Word for the Unrelenting Force shout, Formal recognition as Dragonborn, Free access to High Hrothgar',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:The_Horn_of_Jurgen_Windcaller'
     },
     {
-      quest: 'A Blade in the Dark',
+      ID: 'MQ106',
+      name: 'A Blade in the Dark',
       description: 'Prove you are Dragonborn to Delphine by slaying the dragon Sahloknir.',
-      giver: 'A Mysterious Note',
-      optional: false
+      giver: 'Delphine',
+      location: '   Sleeping Giant Inn, Kynesgrove',
+      level: 1,
+      reward: 'None',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:A_Blade_in_the_Dark'
     },
     {
-      quest: 'Diplomatic Immunity',
+      ID: 'MQ201',
+      name: 'Diplomatic Immunity',
       description: 'Infiltrate a party at the Thalmor Embassy.',
       giver: 'Delphine',
-      optional: false
+      location: 'Riverwood, Solitude, Thalmor Embassy',
+      level: 1,
+      reward: 'None',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Diplomatic_Immunity'
     },
     {
-      quest: 'A Cornered Rat',
+      ID: 'MQ202',
+      name: 'A Cornered Rat',
       description: 'Locate Esbern.',
       giver: 'Delphine',
-      optional: false
+      location: 'Riften, The Ratway',
+      level: 1,
+      reward: 'None',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:A_Cornered_Rat'
     },
     {
-      quest: 'Alduins Wall',
+      ID: 'MQ203',
+      name: 'Alduins Wall',
       description: 'Locate the prophecy within Sky Haven Temple.',
       giver: 'Esbern',
-      optional: false
+      location: 'The Ratway, Riverwood, Sky Haven Temple',
+      level: 1,
+      reward: 'Dragonbane',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Alduin%27s_Wall'
     },
     {
-      quest: 'The Throat of the World',
+      ID: 'MQ204',
+      name: 'The Throat of the World',
       description: 'Receive a lesson from Paarthurnax.',
       giver: 'Arngeir',
-      optional: false
+      location: 'High Hrothgar, Throat of the World',
+      level: 1,
+      reward: 'One unlocked word of power for the Fire Breath shout and the whole Clear Skies shout',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:The_Throat_of_the_World'
     },
     {
-      quest: 'Elder Knowledge',
+      ID: 'MQ205',
+      name: 'Elder Knowledge',
       description: 'Locate an Elder Scroll.',
       giver: 'Paarthurnax',
-      optional: false
+      location: 'Throat of the World, Sky Haven Temple or High Hrothgar, Septimus Signuss Outpost, Alftand, Blackreach, Tower of Mzark.',
+      level: 1,
+      reward: 'None',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Elder_Knowledge'
     },
     {
-      quest: 'Alduins Bane',
+      ID: 'MQ206',
+      name: 'Alduins Bane',
       description: 'Exploit the Time Wound to learn Dragonrend.',
       giver: 'Paarthurnax',
-      optional: false
+      location: 'Throat of the World',
+      level: 1,
+      reward: 'Dragonrend shout',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Alduin%27s_Bane'
     },
     {
-      quest: 'The Fallen',
+      ID: 'MQ301',
+      name: 'The Fallen',
       description: 'Capture and interrogate one of Alduins allies.',
-      giver: 'Paarthurnax, Arngeir, Esbern',
-      optional: false
+      giver: 'Alduin',
+      location: 'Throat of the World, High Hrothgar, and/or Sky Haven Temple, Dragonsreach',
+      level: 1,
+      reward: 'None',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:The_Fallen'
     },
     {
-      quest: 'Paarthurnax (optional)',
+      ID: 'MQPaarthurnax',
+      name: 'Paarthurnax',
       description: 'Deal with a potential threat.',
-      giver: '',
-      optional: false
+      giver: 'Esbern or Delphine',
+      location: 'Sky Haven Temple, Throat of the World',
+      level: 1,
+      reward: 'Free access to Sky Haven Temple',
+      type: 'optional',
+      link: 'https://en.uesp.net/wiki/Skyrim:Paarthurnax_(quest)'
     },
     {
-      quest: 'Season Unending (optional)',
+      ID: 'MQ302',
+      name: 'Season Unending',
       description: 'Enforce a ceasefire to secure Dragonsreach.',
-      giver: '',
-      optional: false
+      giver: 'Jarl Balgruuf the Greater or Vignar Gray-Mane',
+      location: 'None',
+      level: 1,
+      reward: 'Call Dragon Shout',
+      type: 'optional',
+      link: 'https://en.uesp.net/wiki/Skyrim:Season_Unending'
     },
     {
-      quest: 'The World-Eaters Eyrie',
+      ID: 'MQ303',
+      name: 'The World-Eaters Eyrie',
       description: 'Battle through Skuldafn Temple to reach Alduins portal to Sovngarde.',
-      giver: '',
-      optional: false
+      giver: 'Odahviing',
+      location: 'Skuldafn Temple',
+      level: 1,
+      reward: 'Nahkriin',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:The_World-Eater%27s_Eyrie'
     },
     {
-      quest: 'Sovngarde',
+      ID: 'MQ304',
+      name: 'Sovngarde',
       description: 'Seek out Alduin within Sovngarde.',
-      giver: '',
-      optional: false
+      giver: 'Automatic upon entering Sovngarde during The World-Eaters Eyrie.',
+      location: 'Sovngarde',
+      level: 1,
+      reward: 'None',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Sovngarde_(quest)'
     },
     {
-      quest: 'Dragonslayer',
+      ID: 'MQ305',
+      name: 'Dragonslayer',
       description: 'Put an end to the World Eater.',
-      giver: '',
-      optional: false
-    }
-  ],
-  CollegeOfWinterhold: [
-    {
-      quest:'College Objective Quest',
-      description:'Visit the College of Winterhold.',
-      giver:'',
-      optional: false
-    },{
-      quest:'First Lessons',
-      description:'Join the College of Winterhold.',
-      giver:'',
-      optional: false
-    },{
-      quest:'Under Saarthal',
-      description:'Uncover the mysteries beneath this ancient ruin.',
-      giver:'Tolfdir',
-      optional: false
-    },{
-      quest:'Hitting the Books',
-      description:'Find the books needed to identify the artifact found in Saarthal.',
-      giver:'Savos Aren',
-      optional: false
-    },{
-      quest:'Good Intentions',
-      description:'Find and consult the Augur of Dunlain.',
-      giver:'Tolfdir',
-      optional: false
-    },{
-      quest:'Revealing the Unseen',
-      description:'Discover the location of Magnus staff.',
-      giver:'Mirabelle Ervine',
-      optional: false
-    },{
-      quest:'Containment',
-      description:'Deal with the aftermath of the Eyes power surge.',
-      giver:'',
-      optional: false
-    },{
-      quest:'The Staff of Magnus',
-      description:'Retrieve the Staff of Magnus.',
-      giver:'Mirabelle Ervine',
-      optional: false
-    },{
-      quest:'The Eye of Magnus',
-      description:'Reclaim the College and contain the Eye.',
-      giver:'',
-      optional: false
-    },{
-      quest:'',
-      description:'',
-      giver:'',
-      optional: false
+      giver: 'Hakon, Gormlaith, and Felldir',
+      location: 'Hall of Valor, Sovngarde',
+      level: 1,
+      reward: 'Call of Valor',
+      type: 'main',
+      link: 'https://en.uesp.net/wiki/Skyrim:Dragonslayer'
     }
   ]
 }
-
 export default tableData
